c186d6f1adcf870ce3b68dd12994e15ccd5f8277
Alejandro Hernández <alegit2106@gmail.com>
2025-11-21 19:54:24 -0400

    Implementar UserController y rutas para manejar publicaciones

---
+++ src/controllers/mainController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { initializeDB } from '../db';
+
+export class UserController {
+
+  async getMain (req: Request, res: Response){
+    res.status(200).render("layouts/main", { user: req.session.user });
+  }
+
+  async getCreatePost (req: Request, res: Response){
+    res.status(200).render("posts/createPost");
+  }
+
+  async postCreatePost (req: Request, res: Response){
+    const { title, content, category } = req.body; //Requiriendo los datos desde la vista
+    const userID = req.session.user?.id; //Posterior revisión para validar mejor
+
+    if (!userID) {
+      return res.status(401).send("Usuario no autenticado");
+    }
+
+    if (!title || !content || !category) {
+      return res.status(400).send("Faltan datos obligatorios");
+    }
+
+    try{
+      const db = await initializeDB();
+      const stmt = await db.prepare(`INSERT INTO publicaciones (title, content, user_id, category_id) VALUES (?, ?, ?, ?)`);
+      await stmt.run(title, content, userID, category);
+      await stmt.finalize();
+
+      res.status(201).send("Publicación creada exitosamente");
+    } catch (e){
+      console.error(e);
+      res.status(500).send("Error al crear la publicación");
+    }
+  }
+}
--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,9 +1,13 @@
 import { Request, Response, Router } from 'express';
+import { UserController } from '../controllers/mainController';
 
 const router = Router();
+const mainController = new UserController();
 
-router.get("/main", (req: Request, res: Response) => {
-  res.render("layouts/main", { user: req.session.user });
-});
+router.get("/main", (req: Request, res: Response) => mainController.getMain(req, res));
+
+router.get("/createPost", (req: Request, res: Response) => mainController.getCreatePost(req, res))
+
+router.post("/createPost", (req: Request, res: Response) => mainController.postCreatePost(req, res))
 
 export default router;
